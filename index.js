dc3a72ea95f7a6b72b72ecaf2b5553285a224e78
Hamid Ali <hamidalioffice@gmail.com>
2022-02-05 19:50:50 +0530

    midStage

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
+const path = require('path')
+
+const expressEdge = require('express-edge')
+
+
+
 const express = require('express')
 
 const app = new express()
+
+app.use(express.static('public'))
+
+
+app.use(expressEdge)
+
+app.set('views' , `${__dirname}\views`)
+
+app.get('/' , (req , res) => {
+
+    // res.sendFile(path.resolve(__dirname , 'pages/index.html'))
+
+    res.render('index')
+
+})
+
+
+app.get('/about' , (req , res) => {
+
+    res.sendfile(path.resolve(__dirname , 'pages/about.html'))
+
+})
+
+
+app.get('/contact' , (req , res) => {
+
+    res.sendfile(path.resolve(__dirname , 'pages/contact.html'))
+
+})
+
+app.get('/post' , (req , res) => {
+
+    res.sendfile(path.resolve(__dirname , 'pages/post.html'))
+
+})
+
+
+
 
 app.listen(4000 , () => {
 
     console.log("app listening on port 4000 .")
 
 })
-
